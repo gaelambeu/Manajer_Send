d93f48e4366768d622ef981248269fb270e5d410
GAEL AMBEU <ambeugael128@gmail.com>
2023-09-23 01:21:31 +0000

    move

--- pain/App.js
+++ pain/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
+import { Button } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Click me" />
       <StatusBar style="auto" />
     </View>
   );
